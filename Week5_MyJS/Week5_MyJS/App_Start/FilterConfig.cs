7ea7f0905b8e1306d598d62655165a1818d0cd62
iqgnahZ <zwan0348@student.monash.edu>
2022-08-22 12:52:15 +1000

    task5.1

---
+++ Week5_MyJS/Week5_MyJS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Week5_MyJS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Week5_MyJS/Week5_MyJS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week5_MyJS.MvcApplication" Language="C#" %>
